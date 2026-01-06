29902198024d1eb5f65424d7b671148420834983
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-06 20:44:07 +0200

    Fetching random word from the fake db

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((data) => {
+        const randomSolution = data[Math.floor(Math.random() * data.length)];
+        setSolution(randomSolution);
+      });
+  }, [setSolution]);
+
   return (
     <div className="App">
       <h1>Wordle (Lingo) </h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
