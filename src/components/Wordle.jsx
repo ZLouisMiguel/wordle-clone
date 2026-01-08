ff3b573670780ec39b32062999a41fea305bbd8a
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-08 19:42:02 +0200

    Added tracking of more state from the use wordle hook

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -2,13 +2,16 @@ import { useEffect } from "react";
 import useWordle from "../hooks/useWordle.js";
 
 const Wordle = ({ solution }) => {
-  const { currentGuess, handleKeyUp } = useWordle(solution);
+  const { currentGuess, handleKeyUp, guesses, turn , isCorrect } = useWordle(solution);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyUp);
     return () => window.removeEventListener("keyup", handleKeyUp);
   }, [handleKeyUp]);
 
+  useEffect(()=>{
+    console.log(guesses, turn , isCorrect);
+  },[guesses , turn , isCorrect]);
   return (
     <>
       <div>solution - {solution.word}</div>
