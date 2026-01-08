0febc7c9c4b22a085c05dd38d1b63a91c0f5e98b
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-08 20:16:30 +0200

    Initial skeleton for the game grid

---
+++ src/components/Grid.jsx
@@ -0,0 +1,15 @@
+import Row from "./Row";
+
+const Grid = ({ currentGuess, guesses, turn }) => {
+  return (
+    <>
+      <div className="grid">
+        {guesses.map((guess, index) => {
+          return <Row key={index} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Grid;
