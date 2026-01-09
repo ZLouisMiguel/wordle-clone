1c91c79b7c5dfbad67ab4c7220fa7eeac5c6d40e
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-09 08:54:04 +0200

    Mounted the model component to show the user when they've won or lost , and handled its display with the show modal state

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,25 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useWordle from "../hooks/useWordle.js";
 import Grid from "./Grid.jsx";
 import Keypad from "./Keypad.jsx";
+import Modal from "./Modal.jsx";
 
 const Wordle = ({ solution }) => {
   const { currentGuess, handleKeyUp, guesses, turn, isCorrect, usedKeys } =
     useWordle(solution);
 
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     window.addEventListener("keyup", handleKeyUp);
     if (isCorrect) {
-      console.log("Congrats, You Win !");
+      setTimeout(() => {
+        setShowModal(true);
+      }, 2500);
       window.removeEventListener("keyup", handleKeyUp);
     }
 
     if (turn > 5) {
-      console.log("You've exhausted your guesses ");
+      setTimeout(() => {
+        setShowModal(true);
+      }, 2500);
       window.removeEventListener("keyup", handleKeyUp);
     }
     return () => window.removeEventListener("keyup", handleKeyUp);
-  }, [handleKeyUp, isCorrect , turn]);
+  }, [handleKeyUp, isCorrect, turn]);
 
   return (
     <>
@@ -27,6 +34,9 @@ const Wordle = ({ solution }) => {
       <div>Current Guess: - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
       <Keypad usedKeys={usedKeys} />
+      {showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
     </>
   );
 };
