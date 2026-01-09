b8dbdca6d135c2e59d874dfc5d4f2c9235c02ad4
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-09 07:33:39 +0200

    Mounted the keypad component in the main game app flow

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,23 +1,26 @@
 import { useEffect } from "react";
 import useWordle from "../hooks/useWordle.js";
 import Grid from "./Grid.jsx";
+import Keypad from "./Keypad.jsx";
 
 const Wordle = ({ solution }) => {
-  const { currentGuess, handleKeyUp, guesses, turn , isCorrect } = useWordle(solution);
+  const { currentGuess, handleKeyUp, guesses, turn, isCorrect } =
+    useWordle(solution);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyUp);
     return () => window.removeEventListener("keyup", handleKeyUp);
   }, [handleKeyUp]);
 
-  useEffect(()=>{
-    console.log(guesses, turn , isCorrect);
-  },[guesses , turn , isCorrect]);
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect);
+  }, [guesses, turn, isCorrect]);
   return (
     <>
       <div>solution - {solution.word}</div>
       <div>Current Guess: - {currentGuess}</div>
-      <Grid  currentGuess={currentGuess} guesses = {guesses} turn = {turn} />
+      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+      <Keypad />
     </>
   );
 };
